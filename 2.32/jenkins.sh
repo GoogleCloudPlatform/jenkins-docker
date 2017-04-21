f6d8231175e62138e6da984f40b14423e67763fd
Wojciech Grzelak <wgrzelak@google.com>
2017-04-21 04:41:14 -0700

    Delete -u flag from shebang
    Change-Id: I8fc37765f4a3f69e4630a0228357c16f8b573d98

--- 2.32/jenkins.sh
+++ 2.32/jenkins.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eu
+#!/bin/bash
 #
 # Copyright (c) 2017 Google Inc.
 #
@@ -18,6 +18,8 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+set -e
 
 : "${JENKINS_HOME:="/var/jenkins_home"}"
 touch "${COPY_REFERENCE_FILE_LOG}" || { echo "Can not write to ${COPY_REFERENCE_FILE_LOG}. Wrong volume permissions?"; exit 1; }
